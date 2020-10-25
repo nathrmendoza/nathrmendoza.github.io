b7b73c593bdcf6f5ead15a944d06fdd094e9e9be
nathrmendoza <mendoza.nathaniel328@gmail.com>
2020-10-25 21:10:51 +0800

    updated no projects yet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-$(documnet).ready(function(){
+$(document).ready(function(){
     //text animations
     $('.fadein').textillate({
         loop: false,
@@ -10,4 +10,31 @@ $(documnet).ready(function(){
             delay : 40
         }
     });
+    $('.message p').textillate({
+        loop: false,
+        autostart: false,
+        in: {
+            effect: 'fadeIn',
+            delayScale : 1,
+            delay : 50
+        }
+    });
+
+    //godown 
+    $("#arrow-go-down").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#main").offset().top
+        }, 500);
+    });
+
+    var bottomscroll = window.pageYOffset + $(window).height();
+    var runtext = false;
+    $(window).scroll(function(){
+        bottomscroll = window.pageYOffset + $(window).height();
+        if (bottomscroll >=  $('footer').offset().top && !runtext) {
+            runtext=true;
+            $("footer .inner-container").addClass('active');
+            $('.message p').textillate('start');
+        }
+    })
 });
